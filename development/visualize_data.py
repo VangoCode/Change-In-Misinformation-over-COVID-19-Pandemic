7e5e49f54c28ca8d753ef1ad6625c50364d90e1a
elsie zhu <elsiezhu@elsies-MacBook-Air2.local>
2021-12-12 21:17:09 -0500

    Finished writing copyright

--- development/visualize_data.py
+++ development/visualize_data.py
@@ -5,15 +5,11 @@ and Sys Library.
 Copyright and Usage Information
 ==================================================
 
-This file is provided solely for the personal and private use of students
-taking CSC110 at the University of Toronto St. George campus. All forms of
-distribution of this code, whether as given or with any changes, are
-expressly prohibited. For more information on copyright for CSC110 materials,
-please consult our Course Syllabus.
+All forms of distribution of this code, whether as given or with any changes, are
+expressly prohibited.
 
 This file is Copyright (c) 2021 Ron Varshavsky and Elsie (Muhan) Zhu.
 """
-
 import pygame
 import sys
 from pull_tweets_classes import generate_month_year_list, MonthYear
